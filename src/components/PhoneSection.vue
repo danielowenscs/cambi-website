e045f5ef34b82970901e6035b0a55cdfa0f8f3ad
vianc <viancareynoso@gmail.com>
2023-04-12 21:14:41 -0700

    working on the phone section + footer component

--- src/components/PhoneSection.vue
+++ src/components/PhoneSection.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="background">
-<div class="container">
-    <div class="H2">{{ phone1 }}</div>
-    <div class="B1">{{ phone2 }}</div>
-    <button class="B1 button">Join Cambi</button>
-</div>
-<div class="telly">
-    <img src="@/assets/imgs/phone.png">
-</div>
+    <div class="container">
+        <div class="grid">
+            <div class="content">
+                <div class="box">
+                    <div class="innercontainer">
+                        <div class="H2">{{ phone1 }}</div>
+                        <div class="B1">{{ phone2 }}</div>
+                        <button class="button">Join Cambi</button>
+                    </div>
+                    <div class="telly">
+                    <img src="@/assets/imgs/phone.png">
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 <script>
@@ -28,23 +36,66 @@ export default {
 @import '@/assets/styles/styles.scss';
 
 img {
-    position: right;
+    float: inline-end; 
     height: 448px;
     width: 210px;
 }
 
+.telly{
+    float: right;
+}
+
+.box {
+    width: 980px;
+    height: 448px;
+}
+
+.container {
+    max-width: 1260px;
+    margin: auto;
+}
+.grid {
+    padding: 120px 40px;
+    margin: auto;
+    height: 400px;
+    gap: 20px;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+}
+.content {
+    grid-column: 2/11;
+}
+
+.innercontainer {
+    width: 457px;
+}
 .background {
     background: $Se6;
+    height: 688px;
 }
 
 .button {
+    font-family: 'Inter';
+    font-weight: 400;
+    line-height: 24px;
+    font-size: 18px;
+    
     border-radius: 40px;
     width: 159px;
-    //height: 56px;
+    // height: 56px;
     padding: 16px;
     background-color: $Se2;
     border-color: none;
 }
 
+.H2 { 
+    padding-top: 32px;
+    padding-bottom: 32px;
+}
+
+.B1 { 
+    padding-top: 32px;
+    padding-bottom: 32px;
+}
 
 </style>
